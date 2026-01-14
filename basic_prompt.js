153ecdf3f86e434d02af9ba32474fd453f0c632b
WilcoApp <hello@wilco.gg>
2026-01-14 11:31:48 +0000

    Initial commit

--- basic_prompt.js
+++
@@ -1,6 +0,0 @@
-function ToCamelCase(str) {
-    return str
-        .split(' ')
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-        .join('');
-}
--- chain_prompt.js
+++
@@ -1,6 +0,0 @@
-function toKebabCase(str) {
-    return str
-        .replace(/([a-z])([A-Z])/g, '$1-$2') // Insert hyphens before uppercase letters
-        .replace(/[^a-z0-9-]/g, '') // Remove special characters
-        .toLowerCase(); // Convert to lowercase
-}
